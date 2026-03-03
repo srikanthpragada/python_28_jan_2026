0d9efdf79d33c26ed3b5246d48c863fd1c4b9b85
srikanthpragada <srikanthpragada@gmail.com>
2026-03-03 19:51:45 +0530

    03-MAR

--- demo/dbapi/db_to_json.py
+++ demo/dbapi/db_to_json.py
@@ -10,18 +10,17 @@ def emp_to_dict(emp):
     return {"id": emp[0], "name": emp[1], "job": emp[2], "salary": emp[3]}
 
 
-employees = []
 con = sqlite3.connect(dbutil.DBNAME)
 cur = con.cursor()
 cur.execute("select * from employees")  # SQL Command
-
+employees = []
 for emp in cur.fetchall():
-    employees.append(emp_to_dict(emp)) # Tuple to dict
+    employees.append(emp_to_dict(emp))  # Tuple to dict
 
 cur.close()
 con.close()
 
-print(json.dumps(employees)) # list of dict to array of json objects
+print(json.dumps(employees))  # list of dict to array of json objects
 
 # Write to file
 f = open("employees.json", "wt")
